d0fbccb52293d9c70320dc00a941fdd0ebc3b61e
Brian Burrow <bburrow81@gmail.com>
2020-09-17 20:52:56 +0200

    post model updates

--- db/migrate/20200917174601_create_artists.rb
+++ db/migrate/20200917174601_create_artists.rb
@@ -1,8 +1,10 @@
 class CreateArtists < ActiveRecord::Migration[6.0]
   def change
     create_table :artists do |t|
-      t.string :name
+      t.string :name, null: false
       t.string :location
+      t.float  :longitude
+      t.float  :latitude
       t.string :spotify_id
       t.string :facebook_id
 
