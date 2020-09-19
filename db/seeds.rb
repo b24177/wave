ef4fe43da2be4428661df6fafef8f4ab48a1ac1b
Brian Burrow <bburrow81@gmail.com>
2020-09-19 14:51:19 +0200

    seed info created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "deleting prior records"
+User.delete_all
+Artist.delete_all
+Content.delete_all
+Post.delete_all
+
+
+puts 'Creating users...'
+
+user1 = User.create!({
+   email: 'testUser@gmail.com',
+  password: 'password'
+})
+
+artist1 = Artist.create!({
+  name: 'Lust for Youth',
+  location: 'Copenhagen',
+  spotify_id: 1234,
+  facebook_id: 5678,
+})
+
+post1 = Post.create!({
+artist: artist1,
+source: 'Facebook'
+})
+
+content1 = Content.create! ({
+  format: 'text',
+  data: 'Listen to my new song',
+  post: post1
+})
