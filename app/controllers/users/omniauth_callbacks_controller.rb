60768d677e680a2290625a5078eeafc3bc067609
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2020-09-22 21:33:06 +0200

    spotify api

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -31,7 +31,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-     @user = User.from_omniauth(request.env["omniauth.auth"])
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
@@ -40,6 +40,11 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       @user.save!
       # session["devise.spotify_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
       # redirect_to new_user_registration_url
+    end
+    @artists = spotify_user.top_artists
+    @artists.each do |artist|
+      new_artist = Artist.create!(name: artist.name, spotify_id: artist.id) unless Artist.exists?(name: artist.name)
+      UserArtist.create(artist: new_artist, user: @user, status: nil )
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :user_artists
+  has_many :artists, through: :user_artists
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:spotify]
@@ -11,15 +14,15 @@ class User < ApplicationRecord
   end
 
   def self.from_omniauth(auth)
-  where(spotify_id: auth.info.id).first_or_create do |user|
-    user.email = auth.info.email
+    where(spotify_id: auth.info.id).first_or_create do |user|
+      user.email = auth.info.email
 
-    user.password = Devise.friendly_token[0, 20]
-    # user.name = auth.info.name   # assuming the user model has a name
-    # user.image = auth.info.image # assuming the user model has an image
-    # If you are using confirmable and the provider(s) you use validate emails,
-    # uncomment the line below to skip the confirmation emails.
-    # user.skip_confirmation!
+      user.password = Devise.friendly_token[0, 20]
+      # user.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
   end
-end
 end
