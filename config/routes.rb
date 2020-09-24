2eebc40ce110fcee8e278edcbd306dacb14c85c6
Brian Burrow <bburrow81@gmail.com>
2020-09-24 20:38:52 +0200

    artist page updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
-  resources :artists, only: [:index, :show, :update]
+  resources :artists, only: [:index, :show, :update] do
+    member do
+      patch :follow
+    end
+
+  end
   resources :contents, only: [:index, :show]
   get '/posts' => "posts#index", :as => :user_root
 end
