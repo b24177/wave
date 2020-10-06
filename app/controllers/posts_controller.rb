d9fbf43cee0c8dfe75e78ac4b52b19eedc947496
Brant Monaghan <monaghanb@gmail.com>
2020-10-06 21:30:14 +0200

    Added function to clear notifications when number badge is clicked

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    current_user.notifications.unread.update_all(read_at: Time.now)
+    # User.first.notifications.update_all(read_at: nil)
   end
 
   def show
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  scope :unread, -> () {where(read_at: nil)}
 end
