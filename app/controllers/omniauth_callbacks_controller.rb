ca07eabf1f6cfa08fd316dfef1b9f309cc91f17f
Joakim Hunskaar <joakim.borge.hunskar@gmail.com>
2020-10-12 21:48:34 +0200

    fetches spotify artists then connect user with seed data

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -44,8 +44,9 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       # session["devise.spotify_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
       # redirect_to new_user_registration_url
     end
-    follow_seed_artists(@user)
     get_followed_artists(spotify_user)
+    follow_seed_artists(@user)
+
   end
 
   def failure
